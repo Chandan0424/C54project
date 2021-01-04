d22ed0c9761a4c474d7d48af2357c6867514ba88
Chandan0424 <76838260+Chandan0424@users.noreply.github.com>
2021-01-04 20:22:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function Calculate(){
+    var Lenght=Number(document.getElementById("Lenght").value);
+    Number(document.getElementById("Area").innerHTML=Lenght*Lenght);
+    Number(document.getElementById("Perimeter").innerHTML=Lenght*4);
+}
